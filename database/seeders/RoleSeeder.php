cca40ab01773f198840df6c1856e37f39c533d28
Xavi Ortega <xavier.ortegacassou@gmail.com>
2024-02-05 00:27:03 +0100

    bump to laravel 9

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Seeder;
 class RolesSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeders.
      *
      * @return void
      */
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Seeder;
 class UsersSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeders.
      *
      * @return void
      */
